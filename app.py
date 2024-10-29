d689defa4db169171b21cf6870fcd489a5f2dc74
Karen Rincón <karenrinconjm2016@gmail.com>
2024-10-29 11:46:51 -0500

    Manejo de excepciones creación BBDD

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+print("DATABASE_URL:", os.getenv('DATABASE_URL'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
--- setup_database.py
+++ setup_database.py
@@ -1,7 +1,11 @@
 from app import app, db  # Importa la instancia de `app` y `db` desde `app.py`
 from models import Categoria, DatoCurioso
+from sqlalchemy.exc import SQLAlchemyError
 
 # Ejecuta `create_all()` dentro del contexto de la aplicación
 with app.app_context():
-    db.create_all()
-    print("Base de datos creada exitosamente.")
+    try:
+        db.create_all()
+        print("Base de datos creada exitosamente.")
+    except SQLAlchemyError as e:
+        print("Error al crear la base de datos:", str(e))
