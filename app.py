abb877cb07f268ec992623964a95186a4adfb284
Karen Rincón <karenrinconjm2016@gmail.com>
2024-10-29 11:25:37 -0500

    Ajuste para conexión a BBDD en Railway

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from models import db, Categoria, DatoCurioso
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///database.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
